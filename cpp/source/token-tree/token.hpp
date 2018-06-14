91b3cbffecf1dc0d12a73f14679e8190dcbafa93
Giumo Clanjor (哆啦比猫/兰威举) <cjxgm2@gmail.com>
2018-06-15 00:49:45 +0800

    token: Add convenience functions for iteration

--- cpp/source/token-tree/token.hpp
+++ cpp/source/token-tree/token.hpp
@@ -35,6 +35,13 @@ namespace cctt
         Token(char const* first, char const* last, Token_Tag_Set tags)
             : first{first}, last{last}, tags{tags}
         {}
+
+        auto is_end() const -> bool { return tags.has_all_of(Token_Tag::end); }
+        auto is_leaf() const -> bool { return (pair == nullptr); }
+        auto child() const -> Token const* { return (pair > this + 1 ? this + 1 : nullptr); }
+        auto last_child() const -> Token const* { return (pair > this + 1 ? pair : nullptr); }
+        auto closing_pair() const -> Token const* { return (pair > this ? pair : this); }
+        auto next() const -> Token const* { return closing_pair() + 1; }
     };
 
     static_assert(sizeof(Token) <= 64, "Token is too big to fit into a typical cacheline.");
